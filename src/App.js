36d9780abc6b9dca97d9c7bd3f83bb99a2035360
Fermin <fermin.hdez@gmail.com>
2022-05-08 22:44:11 +0200

    update text

--- src/App.js
+++ src/App.js
@@ -13,11 +13,12 @@ export default function App() {
 
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+        🦄 WOP WOP! ☕️
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Fermin and I worked on self-driving cars so that's pretty cool right? 
+        Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
