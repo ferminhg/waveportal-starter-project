56fbd0f4a91058d235f22372efcdef35cc3feeba
Fermin <fermin.hdez@gmail.com>
2022-05-09 22:39:45 +0200

    fix errors and ask account

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
@@ -7,22 +7,47 @@ const App = () => {
   */
   const [currentAccount, setCurrentAccount] = useState("");
   const checkIfWalletIsConnected = async () => {
-    const { ethereum } = window;
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-      return;
-    }
-    console.log("We have the ethereum object", ethereum);
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      }
+      console.log("We have the ethereum object", ethereum);
 
-    const accounts = await ethereum.request({ method: "eth_accounts" });
+      const accounts = await ethereum.request({ method: "eth_accounts" });
 
-    if (accounts.length === 0) {
-      console.log("No authorized account found");
-      return;
+      if (accounts.length === 0) {
+        console.log("No authorized account found");
+        return;
+      }
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+    } catch (error) {
+      console.log(error);
     }
-    const account = accounts[0];
-    console.log("Found an authorized account:", account);
-    setCurrentAccount(account)
+  }
+
+  /**
+  * Implement your connectWallet method here
+  */
+   const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   useEffect(() => {
@@ -43,6 +68,15 @@ const App = () => {
         <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
