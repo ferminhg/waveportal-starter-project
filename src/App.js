b5c019bd8b09fd4647f63169215ddd5ceda9d597
Fermin <fermin.hdez@gmail.com>
2022-05-09 22:34:40 +0200

    access eth accounts

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React, { useEffect } from "react";
 import "./App.css";
 
 const App = () => {
+  /*
+   * Just a state variable we use to store our user's public wallet.
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
   const checkIfWalletIsConnected = () => {
-    /*
-    * First make sure we have access to window.ethereum
-    */
     const { ethereum } = window;
-
     if (!ethereum) {
       console.log("Make sure you have metamask!");
-    } else {
-      console.log("We have the ethereum object", ethereum);
+      return;
     }
+    console.log("We have the ethereum object", ethereum);
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length === 0) {
+      console.log("No authorized account found");
+      return;
+    }
+    const account = accounts[0];
+    console.log("Found an authorized account:", account);
+    setCurrentAccount(account)
   }
 
-  /*
-  * This runs our function when the page loads.
-  */
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
@@ -26,11 +33,11 @@ const App = () => {
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+        👋 WOP WOP !!!
         </div>
 
         <div className="bio">
-          I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am @fermin and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={null}>
